14b2ea973b6dfe1a51fceb62dc3d149748bbfc82
wayne <world.wayno@gmail.com>
2015-11-02 22:56:01 -0500

    adding various nuget packages

---
+++ ScribeAdminPortal/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+//[assembly: OwinStartup(typeof(ScribeAdminPortal.App_Start.Startup))]
+
+namespace ScribeAdminPortal.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("default", "{controller}");
+            app.UseWebApi(config);
+        }
+    }
+}
